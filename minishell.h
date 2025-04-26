de8f4faed157545063c66cd2528d9f507f97bd4f
choco798 <choco798>
2025-04-26 16:20:26 +0900

    #1 split word

---
+++ minishell.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: somukaid <somukaid@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 15:10:25 by somukaid          #+#    #+#             */
+/*   Updated: 2025/04/24 15:37:25 by somukaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include "libft/libft.h"
